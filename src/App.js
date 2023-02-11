9416845216d9a837f6a2e609d7a5e3fc6d6e8491
daniellehemet <dmhemet@hotmail.ca>
2023-02-10 20:06:25 -0500

    light and dark mode toggle function created, styled checkbox to sliding toggle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import './darkMode.css';
 
 function App() {
+  // theme state for light/dark mode
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    if (theme === 'light'){
+      setTheme('dark')
+    } else {
+      setTheme('light');
+    }
+  };
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${theme}`}>
+      <p>Testing light/dark mode</p>
+      <div className='toggleSlot'>
+        <label htmlFor="themeToggle" className='label'>
+          <input type="checkbox" name="themeToggle" id="themeToggle" className="toggleCheckbox" onChange={toggleTheme}></input>
+          <div className='toggleSlider'></div>
+        </label>
+      </div>
     </div>
   );
 }
